3ab5118497ba0e14ef49e16c35cf5e4d7ffca3d9
vini230403 <vini2003.melo@gmail.com>
2025-04-08 22:46:18 -0300

    arrumando slide e modal

--- main.js
+++ main.js
@@ -63,6 +63,10 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-pagination',
         clickable: true
     },
+    autoplay: {
+      delay: 3000, 
+      disableOnInteraction: false, 
+    },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
